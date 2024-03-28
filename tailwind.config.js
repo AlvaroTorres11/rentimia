cd739eee0d1bb866c7714d2841730b496cbec0dc
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-03-28 17:16:49 -0600

    config: color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ export default {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["winter", "sunset"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: [
+      "winter",
+      "sunset",
+      {
+        winter: {
+          //? Con esto podemos modificar colores del tema
+          ...require("daisyui/src/theming/themes")["winter"],
+          primaryColor50: "#ecfdf5",
+          primaryColor100: "#d1fae4",
+          primaryColor200: "#a7f3cf",
+          primaryColor300: "#6ee7b5",
+          primaryColor400: "#52daa6",
+          primaryColor500: "#0fba7e",
+          primaryColor600: "#059666",
+          primaryColor700: "#047855",
+          primaryColor800: "#065f44",
+          primaryColor900: "#064e3a",
+          primaryColor950: "#022c21",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "sunset", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
