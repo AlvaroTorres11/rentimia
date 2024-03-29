50b8caf38de62cd32dbd462d7edb3142efc975b7
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-03-29 17:42:17 -0600

    feat: change text and background colors from themes and add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,26 +3,125 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundColor: {
-        "primaryColor50": "#ecfdf5",
-        "primaryColor100": "#d1fae4",
-        "primaryColor200": "#a7f3cf",
-        "primaryColor300": "#6ee7b5",
-        "primaryColor400": "#52daa6",
-        "primaryColor500": "#0fba7e",
-        "primaryColor600": "#059666",
-        "primaryColor700": "#047855",
-        "primaryColor800": "#065f44",
-        "primaryColor900": "#064e3a",
-        "primaryColor950": "#022c21",
-      }
-      }
+      fontFamily: {
+        "principal-bold": ["font-principal-bold"],
+        "principal-medium": ["font-principal-medium"],
+        "principal-black": ["font-principal-black"],
+        "principal-heavy": ["font-principal-heavy"],
+        "principal-light": ["font-principal-light"],
+        "principal-semibold": ["font-principal-semibold"],
+        "principal-thin": ["font-principal-thin"],
+        "principal-ultralight": ["font-principal-ultralight"],
+      },
     },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
       "winter",
       "sunset",
+      {
+        winter: {
+          ...require("daisyui/src/theming/themes")["winter"],
+          ".primary-text-color": {
+            color: "#333232",
+          },
+          ".secondary-text-color": {
+            color: "#494949",
+          },
+          ".tertiary-text-color": {
+            color: "#646464",
+          },
+          ".quaternary-text-color": {
+            color: "#8E8E8E",
+          },
+
+          ".primary-color-50": {
+            background: "#ecfdf5",
+          },
+          ".primary-color-100": {
+            background: "#d1fae4",
+          },
+          ".primary-color-200": {
+            background: "#a7f3cf",
+          },
+          ".primary-color-300": {
+            background: "#6ee7b5",
+          },
+          ".primary-color-400": {
+            background: "#52daa6",
+          },
+          ".primary-color-500": {
+            background: "#0fba7e",
+          },
+          ".primary-color-600": {
+            background: "#059666",
+          },
+          ".primary-color-700": {
+            background: "#047855",
+          },
+          ".primary-color-800": {
+            background: "#065f44",
+          },
+          ".primary-color-900": {
+            background: "#064e3a",
+          },
+          ".primary-color-950": {
+            background: "#022c21",
+          },
+        },
+      },
+      {
+        sunset: {
+          ...require("daisyui/src/theming/themes")["sunset"],
+          ".primary-text-color": {
+            color: "#FEFEFE",
+          },
+          ".secondary-text-color": {
+            color: "#EAEAEA",
+          },
+          ".tertiary-text-color": {
+            color: "#CCCCCC",
+          },
+          ".quaternary-text-color": {
+            color: "#BBBBBB",
+          },
+
+          ".primary-color-50": {
+            background: "#022c21",
+          },
+          ".primary-color-100": {
+            background: "#064e3a",
+          },
+          ".primary-color-200": {
+            background: "#065f44",
+          },
+          ".primary-color-300": {
+            background: "#047855",
+          },
+          ".primary-color-400": {
+            background: "#059666",
+          },
+          ".primary-color-500": {
+            background: "#0fba7e",
+          },
+          ".primary-color-600": {
+            background: "#52daa6",
+          },
+          ".primary-color-700": {
+            background: "#6ee7b5",
+          },
+          ".primary-color-800": {
+            background: "#a7f3cf",
+          },
+          ".primary-color-900": {
+            background: "#d1fae4",
+          },
+          ".primary-color-950": {
+            background: "#ecfdf5",
+          },
+        },
+      },
     ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "sunset", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
