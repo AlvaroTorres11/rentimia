9ec01a4bda93a448fe4612144f6e86b5e71ada92
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-03-29 12:41:26 -0600

    feat: implements signin structure and styles

--- src/common/FormComponents.jsx
+++ src/common/FormComponents.jsx
@@ -3,12 +3,20 @@ import { useField } from "formik";
 export function InputField({ label, ...props }) {
   const [field, meta] = useField(props);
   return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
+    <div className="flex flex-col text-start">
+      <label className="text-sm" htmlFor={props.id || props.name}>
+        {label}
+      </label>
+      <input
+        className={`input input-bordered w-full max-w-xs ${
+          meta.error && meta.touched ? "input-error" : ""
+        }`}
+        {...field}
+        {...props}
+      />
       {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
+        <div className="text-red-500 text-sm">{meta.error}</div>
       ) : null}
-    </>
+    </div>
   );
 }
--- src/pages/auth/sign-in/SignIn.jsx
+++ src/pages/auth/sign-in/SignIn.jsx
@@ -2,74 +2,81 @@ import GOOGLE_ICON from "../../../assets/icons/google.png";
 import { InputField } from "../../../common/FormComponents";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
+import HERO from "../../../assets/auth/Signin.jpg";
 
 function SignIn() {
   return (
-    <section>
-      <article>
-        <div>
-          <h2>Bienvenido de nuevo</h2>
-          <span>Inicia sesión en tu cuenta</span>
+    <section className="h-screen flex">
+      <article className="w-full p-24 relative grid place-content-center md:w-3/6 md:p-5 lg:w-2/6 lg:p-10 xl:p-16">
+        <h2 className="absolute top-10 left-10 text-2xl font-bold">Rentimia</h2>
+        <div className="flex flex-col gap-8">
+          <div className="text-start flex flex-col">
+            <h2 className="text-3xl font-semibold">Bienvenido de nuevo</h2>
+            <span>Inicia sesión en tu cuenta</span>
+          </div>
+
+          <div>
+            <button className="btn w-full">
+              <img src={GOOGLE_ICON} alt="Google icon" />
+              Continuar con Google
+            </button>
+          </div>
+
+          <form action="" onSubmit="">
+            <Formik
+              initialValues={{
+                email: "",
+                password: "",
+              }}
+              validationSchema={Yup.object({
+                email: Yup.string()
+                  .min(
+                    5,
+                    "La dirección de correo debe tener al menos 5 caracteres"
+                  )
+                  .email("Direccion de correo no válida")
+                  .required("Debes ingresar un correo electrónico"),
+                password: Yup.string()
+                  .min(20, "La contraseña debe tener como mínimo 8 caracteres")
+                  .max(40, "La contraseña no puede tener más de 40 caracteres")
+                  .required("Debes ingresar una contraseña"),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              <Form className="flex flex-col gap-3">
+                <InputField
+                  label="Correo electrónico"
+                  name="email"
+                  type="text"
+                  placeholder="Ej. efrain@gmail.com"
+                />
+
+                <InputField
+                  label="Contraseña"
+                  name="password"
+                  type="password"
+                  placeholder="**********"
+                />
+
+                <button className="btn primaryColor50" type="submit">
+                  Submit
+                </button>
+              </Form>
+            </Formik>
+          </form>
         </div>
-
-        <div>
-          <button className="btn">
-            <img src={GOOGLE_ICON} alt="Google icon" />
-            Continuar con Google
-          </button>
-          <hr />
-        </div>
-
-        <form action="" onSubmit="">
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .min(
-                  5,
-                  "La dirección de correo debe tener al menos 5 caracteres"
-                )
-                .email("Direccion de correo no válida")
-                .required("Required"),
-              password: Yup.string()
-                .min(20, "La contraseña debe tener como mínimo 8 caracteres")
-                .max(40, "La contraseña no puede tener más de 40 caracteres")
-                .required("Required"),
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-          >
-            <Form>
-              <InputField
-                label="Correo electrónico"
-                name="email"
-                type="text"
-                placeholder="Ej. efrain@gmail.com"
-              />
-
-              <InputField
-                label="Contraseña"
-                name="password"
-                type="password"
-                placeholder="**********"
-              />
-
-              <button type="submit">Submit</button>
-            </Form>
-          </Formik>
-        </form>
       </article>
 
-      <article>
-        <span>Renta lo que necesites y como lo necesites</span>
-        <img src="" alt="" />
+      <article className="w-4/6 hidden md:block">
+        <span className="text-slate-200 text-2xl font-light absolute top-20">
+          Renta lo que necesites y como lo necesites
+        </span>
+        <img className="h-full object-cover" src={HERO} alt="" />
       </article>
     </section>
   );
